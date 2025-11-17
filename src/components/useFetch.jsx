78cce088029802ec216ec95a623538ae72d9b244
saidburhon <saidburhoncoder@gmail.com>
2025-11-17 12:38:55 +0500

    fix fetch func

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -1,63 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-	
-	const [isLoading, setIsLoading] = useState(true)
-	const [error, setError] = useState(null)
-	const [data, setData] = useState(null)
-	
-	useEffect(() => {
-		const fetchData = async () => {
-			setIsLoading(true)
-			setError(null)
-			
-			try {
-				const request = await fetch(url)
-				const data = await request.json()
-				setData(data)
-				
-				if (!request.ok) {
-					throw new Error('Request failed with status ' + request.status)
-				}
-			} catch (e) {
-				setError(e)
-				setData(null)
-			} finally {
-				setIsLoading(false)
-			}
-		}
-		
-		fetchData()
-	}, [url])
-	
-	
-	async function refetch({ params }) {
-		setIsLoading(true)
-		setError(null)
-		
-		try {
-			const request = await fetch(url + '?' + new URLSearchParams(params))
-			const data = await request.json()
-			setData(data)
-			
-			if (!request.ok) {
-				throw new Error('Request failed with status ' + request.status)
-			}
-		} catch (e) {
-			setIsLoading(false)
-			setError(e)
-			setData(null)
-			console.error(e.message)
-		} finally {
-			setIsLoading(false)
-		}
-	}
-	
-	return {
-		data: error ? null : data,
-		isLoading,
-		error,
-		refetch
-	}
-	
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  async function fetchData({ params } = {}) {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const searchParams = new URLSearchParams(params);
+      const req = params ? `${url}?${searchParams}` : url;
+
+      const request = await fetch(req);
+      const data = await request.json();
+      setData(data);
+
+      if (!request.ok) {
+        throw new Error("Request failed with status " + request.status);
+      }
+    } catch (e) {
+      setIsLoading(false);
+      setError(e);
+      setData(null);
+      console.error(e.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {
+    data: error ? null : data,
+    isLoading,
+    error,
+    refetch: fetchData,
+  };
+};
